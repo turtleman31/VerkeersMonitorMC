580d788fab5e0335aa081b703515238288d07dad
Jonathan <146923602+turtleman31@users.noreply.github.com>
2026-09-21 12:53:04 +0200

    Replace Default Template With C Main() Skeleton

---
+++ Verkeersmonitor/src/main.c
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <avr/io.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+int main(void)
+{
+    init(); // arduino core, needed for millis()
+
+    for (;;)
+    {
+    }
+
+    return 0;
+}
